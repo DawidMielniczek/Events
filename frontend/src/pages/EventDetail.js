125d21b66d734d4153c98d7ce650a5caacf39d14
Dawid <dawid.mielniczek99@gmail.com>
2025-06-21 22:27:24 +0200

    Add a new routing

---
+++ frontend/src/pages/EventDetail.js
@@ -0,0 +1,7 @@
+import { useParams } from "react-router-dom"
+
+export default function EventDetailPage(){
+    const params = useParams();
+
+    return <h1> Welcome to Event detail page about id: {params.id}</h1>
+}
---
+++ frontend/src/pages/EventsRoot.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import EventsNavigation from "../components/EventsNavigation";
+
+export default function EventsRoot(){
+    return (
+        <>
+        <main>
+            <EventsNavigation />
+        </main>
+        <Outlet />
+        </>
+    );
+}
