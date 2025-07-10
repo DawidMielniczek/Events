2f8a4137becbc3cb1045c915aa8c6eb68e929681
Dawid <dawid.mielniczek99@gmail.com>
2025-07-10 22:32:01 +0200

    add delete function

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,22 +1,35 @@
-import { useLoaderData } from "react-router-dom";
+import { redirect, useLoaderData } from "react-router-dom";
 import EventItem from '../components/EventItem';
 
-export default function EventDetailPage(){
+export default function EventDetailPage() {
     const data = useLoaderData();
 
     return <EventItem event={data.event} />
 }
 
-export async function loader({request, params}) {
+export async function loader({ request, params }) {
 
     const id = params.id;
-    const resposne = await fetch('http://localhost:8080/events/'+id);
+    const resposne = await fetch('http://localhost:8080/events/' + id);
 
-    if(!resposne.ok){
-        throw new resposne(JSON.stringify({message: 'Could not fetch details for selected events.'}),
-      {status: 500});
-    } else{
+    if (!resposne.ok) {
+        throw new Response(JSON.stringify({ message: 'Could not fetch details for selected events.' }),
+            { status: 500 });
+    } else {
         return resposne;
     }
-    
+}
+
+export async function action({ params, request }) {
+    const eventId = params.id;
+    const response = await fetch('http://localhost:8080/events/' + eventId,{
+        method: request.method,
+    });
+
+    if (!response.ok) {
+        throw new Response(JSON.stringify({ message: 'Could not detelete item.' }),
+            { status: 500 });
+    } else {
+        return redirect('/events');
+    }
 }
